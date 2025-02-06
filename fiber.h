b99b9de2955ae4006deadf4ae834941735c4f672
Carter Williams <carterww@hotmail.com>
2025-02-05 18:28:20 -0600

    Forgot to increment version, oops

--- fiber.h
+++ fiber.h
@@ -9,7 +9,7 @@
 
 /** --- VERSION --- **/
 #define FIBER_VERSION_MAJOR (0)
-#define FIBER_VERSION_MINOR (3)
+#define FIBER_VERSION_MINOR (4)
 #define FIBER_VERSION_PATCH (0)
 
 struct fiber_version {
