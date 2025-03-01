d5f821d611ba301d295226d7bbb2f66dd01287e9
Carter Williams <carterww@hotmail.com>
2025-02-28 23:43:42 -0600

    Add fifo_init_mutex_error fault injection test suite

--- test/test.mk
+++ test/test.mk
@@ -46,9 +46,8 @@ $(TEST_QUEUE_FIFO_INIT_MALLOC_ERROR)_DEPS = $(TEST_QUEUE_FIFO_COMMON_DEPS) \
 	                             	    $(TEST_MOCK_ALLOC_BUILD_DIR)/alloc_fault.o \
 				            $(TEST_QUEUE_FIFO_BUILD_DIR)/fifo_init_malloc_error.o
 
-# $(TEST_QUEUE_FIFO_INIT_MUTEX_ERROR)_DEPS = $(filter-out build/$(THREADING_OBJ), $(TEST_QUEUE_FIFO_COMMON_DEPS)) \
-# 	                                   $(TEST_MOCK_THREADING_BUILD_DIR)/threading_trace_fault.o \
-# 	                                   $(TEST_QUEUE_BUILD_DIR)/fifo/fifo_init_mutex_error.o
+$(TEST_QUEUE_FIFO_INIT_MUTEX_ERROR)_DEPS = $(TEST_QUEUE_FIFO_COMMON_DEPS) \
+	                                   $(TEST_QUEUE_BUILD_DIR)/fifo/fifo_init_mutex_error.o
 
 $(TEST_QUEUE_FIFO_INIT_SEM_ERROR)_DEPS = $(TEST_QUEUE_FIFO_COMMON_DEPS) \
 	                                 $(TEST_QUEUE_FIFO_BUILD_DIR)/fifo_init_sem_error.o
@@ -66,7 +65,8 @@ $(TEST_API_INIT)_DEPS = $(TEST_ALL_DEPS) $(TEST_API_BUILD_DIR)/fiber_init.o \
 $(TEST_API_LIBVERSION_COMPAT)_DEPS = $(TEST_ALL_DEPS) $(TEST_API_BUILD_DIR)/fiber_libversion_compatible.o \
 				     $(filter-out %version.o, $(TEST_COMMON_DEPS))
 
-TEST_QUEUE_ALL = $(TEST_QUEUE_FIFO_INIT) $(TEST_QUEUE_FIFO_INIT_MALLOC_ERROR) $(TEST_QUEUE_FIFO_INIT_SEM_ERROR)
+TEST_QUEUE_ALL = $(TEST_QUEUE_FIFO_INIT) $(TEST_QUEUE_FIFO_INIT_MALLOC_ERROR) $(TEST_QUEUE_FIFO_INIT_SEM_ERROR) \
+		 $(TEST_QUEUE_FIFO_INIT_MUTEX_ERROR)
 TEST_API_ALL   = $(TEST_API_CAPABILITY) $(TEST_API_INIT) $(TEST_API_LIBVERSION_COMPAT)
 
 TEST_ALL = $(TEST_QUEUE_ALL) $(TEST_API_ALL)
